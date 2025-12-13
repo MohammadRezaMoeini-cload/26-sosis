5e95cf920f4fab68099429fa87b2d230d4281083
MohammadRezaMoeini-cload <62797771+MohammadRezaMoeini-cload@users.noreply.github.com>
2025-12-13 20:05:07 +0330

    Delete src directory

--- src/components/metronome/metronome test/components/assets/singerIcon.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { PiMicrophoneStageLight } from "react-icons/pi";
-export const singerIcon = () => {
-    return (
-       <PiMicrophoneStageLight size={72}/>
-
-    );
-};
